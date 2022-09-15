286004ed4347004d708dd66c87853ccb8bc62b3b
Umer Khan <umer.khan@lab.mobi>
2022-09-15 22:33:41 +0300

    ChartView

--- WhatsNewiOS16/ChartView.swift
+++ WhatsNewiOS16/ChartView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import Charts
+
+struct Sale: Identifiable {
+    var id: String { UUID().uuidString }
+    let month: String
+    let revenue: Int
+
+    static let dummyData = [
+    Sale(month: "Jan", revenue: 95),
+    Sale(month: "Feb", revenue: 66),
+    Sale(month: "Mar", revenue: 58)
+    ]
+}
 
 struct ChartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(Sale.dummyData) { sale in
+            BarMark(x: .value("Month", sale.month), y: .value("Revenue", sale.revenue))
+        }
+        .foregroundColor(.red)
+        .opacity(0.8)
+        .padding()
     }
 }
 
