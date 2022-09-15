9ffcc7fce770274c2b8037b437369d583d2425ac
Umer Khan <umer.khan@lab.mobi>
2022-09-15 22:31:27 +0300

    chartView

---
+++ WhatsNewiOS16/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  WhatsNewiOS16
+//
+//  Created by Umer Khan on 15.09.2022.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
