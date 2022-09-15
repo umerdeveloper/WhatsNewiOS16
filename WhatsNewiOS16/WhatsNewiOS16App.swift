67150b94a24711e46d285a0553fb9d479ab29ad9
Umer Khan <umer.khan@lab.mobi>
2022-09-15 22:16:50 +0300

    Initial Commit

---
+++ WhatsNewiOS16/WhatsNewiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNewiOS16App.swift
+//  WhatsNewiOS16
+//
+//  Created by Umer Khan on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNewiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
